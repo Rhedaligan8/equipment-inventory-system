59b336b503e51a8c0589396e1504321bccc3d4d7
DevKenny1 <mikekennyjaybacabis@gmail.com>
2025-03-21 15:40:35 +0800

    feat: add user menu to the structure

--- resources/views/livewire/header.blade.php
+++ resources/views/livewire/header.blade.php
@@ -1,3 +1,4 @@
-<div>
-    {{-- Success is as dangerous as failure. --}}
+<div class="p-4 bg-base-100 border-b shadow-md flex justify-between">
+    <h1 class="text-lg font-bold font-inter">PHILIPPINE NUCLEAR RESEARCH INSTITUTE</h1>
+    <livewire:user-menu />
 </div>
