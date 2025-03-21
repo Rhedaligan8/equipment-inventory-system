ad216e66d2189dc5a020d35d016996eb2a1ccdb0
devken4u <devken4u@gmail.com>
2025-03-22 07:50:30 +0800

    feat: add ui and functionality (logout to the user menu)

--- app/Http/livewire/UserMenu.php
+++ app/Http/livewire/UserMenu.php
@@ -3,9 +3,24 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
-
+use Illuminate\Support\Facades\Auth;
+use App\Models\Employee;
 class UserMenu extends Component
 {
+    public $user_fullname;
+
+    public function fetchUserEmployeeInformation()
+    {
+        $user_employee_information = Employee::find(Auth::user()->employee_id);
+        $this->user_fullname = $user_employee_information->lastname . ', ' . $user_employee_information->firstname;
+    }
+
+    public function logout()
+    {
+        Auth::logout();
+        return redirect()->route("login");
+    }
+
     public function render()
     {
         return view('livewire.user-menu');
--- resources/views/livewire/user-menu.blade.php
+++ resources/views/livewire/user-menu.blade.php
@@ -1,3 +1,51 @@
-<div>
-    {{-- Knowing others is intelligence; knowing yourself is true wisdom. --}}
+<div wire:init="fetchUserEmployeeInformation()" class="flex gap-2 items-center">
+    @if (!$user_fullname)
+        <div class="skeleton h-5 w-52"></div>
+        <div class="skeleton h-5 w-16"></div>
+    @else
+        <p class="font-bold"> {{ $user_fullname }}</p>
+        <div class="dropdown dropdown-end">
+            <div tabindex="0" role="button" class="btn btn-sm btn-ghost flex items-center btn-circle">
+                <i class="bi bi-chevron-down"></i>
+            </div>
+            <ul tabindex="0"
+                class="dropdown-content menu bg-base-100 rounded-box z-1 max-w-80 w-80 p-4 shadow-sm border border-base-300">
+                <!-- avatar -->
+                <div class="flex gap-2 items-center">
+                    <!-- circle -->
+                    <div class="avatar avatar-placeholder shrink-0">
+                        <div class="bg-accent text-neutral-content w-12 rounded-full">
+                            <span class="text-lg font-bold">UI</span>
+                        </div>
+                    </div>
+                    <!-- text information -->
+                    <div class="grow w-40">
+                        <p class="font-bold truncate">Halnin, Christopherssssssssssssssssssss</p>
+                        <p>icteiis-admin</p>
+                    </div>
+                    <div class="shrink-0">
+                        <!-- account role -->
+                        @if(Auth::user()->role === 1)
+                            <div class="badge badge-success font-bold">Admin</div>
+                        @elseif(Auth::user()->role === 0)
+                            <div class="badge badge-neutral font-bold">Staff</div>
+                        @endif
+                    </div>
+                </div>
+                <div class="divider"></div>
+                <!-- buttons -->
+                <div class="flex flex-col">
+                    <a href="" class="btn btn-ghost font-bold text-left justify-start">
+                        <i class="bi bi-person-fill-gear"></i>
+                        Account Settings
+                    </a>
+                    <button wire:click="logout()" class="btn  btn-ghost font-bold text-left justify-start">
+                        <i class="bi bi-box-arrow-left"></i>
+                        Logout
+                    </button>
+                </div>
+
+            </ul>
+        </div>
+    @endif
 </div>
