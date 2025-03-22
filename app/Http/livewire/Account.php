7c889f10619fe1e058a46d02201771133fe1900c
devken4u <devken4u@gmail.com>
2025-03-22 21:19:38 +0800

    update account tab

--- app/Http/livewire/Account.php
+++ app/Http/livewire/Account.php
@@ -2,10 +2,31 @@
 
 namespace App\Http\Livewire;
 
+use Illuminate\Support\Facades\Auth;
 use Livewire\Component;
+use App\Models\User;
 
 class Account extends Component
 {
+    public $user_information;
+    public $initials;
+    public $user_fullname;
+    public $division_code;
+    public $unit_code;
+    public $division_desc;
+    public $unit_desc;
+
+    public function getUserInformation()
+    {
+        $this->user_information = User::with("employee")->find(Auth::user()->user_id);
+        $this->initials = strtoupper($this->user_information->employee->lastname[0] . $this->user_information->employee->firstname[0]);
+        $this->user_fullname = $this->user_information->employee->lastname . ', ' . $this->user_information->employee->firstname;
+        $this->unit_code = $this->user_information->employee->unit->unit_code;
+        $this->division_code = $this->user_information->employee->unit->division->division_code;
+        $this->unit_desc = $this->user_information->employee->unit->unit_desc;
+        $this->division_desc = $this->user_information->employee->unit->division->division_desc;
+    }
+
     public function render()
     {
         return view('livewire.account');
--- resources/views/livewire/account.blade.php
+++ resources/views/livewire/account.blade.php
@@ -1,3 +1,32 @@
-<div>
-    {{-- Be like water. --}}
+<div wire:init="getUserInformation()" class="flex flex-col items-center">
+    @if($user_information)
+        <div class="flex flex-col items-center gap-2">
+            <div class="avatar avatar-placeholder">
+                <div class="text-neutral-content w-32 rounded-full bg-accent">
+                    <span class="text-4xl font-bold font-inter">{{ $initials }}</span>
+                </div>
+            </div>
+            <h1 class="text-center font-bold text-xl">{{ $user_fullname }}</h1>
+            @if(Auth::user()->role === 1)
+                <div class="badge badge-success font-bold">Admin</div>
+            @elseif(Auth::user()->role === 0)
+                <div class="badge badge-neutral font-bold">Staff</div>
+            @endif
+        </div>
+        <div class="mt-4 flex flex-col gap-2">
+            <div>
+                <p>Username</p>
+                <p class="w-80 font-bold border rounded-md p-2">{{ $user_information->username }}</p>
+            </div>
+            <div>
+                <p>Division / Section</p>
+                <div class="w-80 font-bold border rounded-md p-2 flex items-center justify-between">
+                    <span>{{ $division_code }} / {{ $unit_code }}</span>
+                    <div class="tooltip" data-tip="{{ $division_desc }} / {{ $unit_desc }}">
+                        <i class="bi bi-info-circle-fill text-accent"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+    @endif
 </div>
