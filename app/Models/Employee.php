656e87e3acab187d2d84ec580d5598b7aaf2b464
devken4u <devken4u@gmail.com>
2025-03-22 17:06:48 +0800

    feat: add relationship to the user and employee

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
 
 class Employee extends Model
 {
@@ -10,4 +11,9 @@ class Employee extends Model
     protected $table = 'employee';
     protected $primaryKey = 'employee_id';
     public $timestamps = false;
+
+    public function user()
+    {
+        return $this->hasOne(User::class, 'employee_id', 'employee_id');
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use App\Models\Employee;
 
 class User extends Authenticatable
 {
@@ -47,4 +48,9 @@ class User extends Authenticatable
         'created_at' => 'date',
         'updated_at' => 'date',
     ];
+
+    public function employee()
+    {
+        return $this->belongsTo(Employee::class, 'employee_id', 'employee_id');
+    }
 }
