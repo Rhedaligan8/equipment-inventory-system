ca6514b8820306b6f2a158be8a7199309eb5451f
devken4u <devken4u@gmail.com>
2025-03-23 21:02:11 +0800

    feat: add cloak to x-show elemenets

--- resources/views/livewire/dashboard.blade.php
+++ resources/views/livewire/dashboard.blade.php
@@ -3,27 +3,40 @@
     <div class="grow flex flex-col">
         <livewire:header />
         <div class="m-6 bg-base-100 grow border-2 border-neutral/25 rounded-md p-4 shadow-md">
-            <section x-show="activeTab == 'equipments'">
+            <!-- equipments tab -->
+            <section class="h-full" x-cloak x-show="activeTab == 'equipments'">
                 <livewire:equipments />
             </section>
 
-            <section x-show="activeTab == 'equipment_types'">
+            <!-- equipments types tab -->
+            <section class="h-full" x-cloak x-show="activeTab == 'equipment_types'">
                 <livewire:equipment-types />
             </section>
 
-            <section x-show="activeTab == 'locations'">
+            <!-- locations tab -->
+            <section class="h-full" x-cloak x-show="activeTab == 'locations'">
                 <livewire:locations />
             </section>
 
-            <section x-show="activeTab == 'users'">
-                <livewire:users />
+            <!-- users tab -->
+            <section class="h-full" x-cloak x-show="activeTab == 'users'">
+                <div x-data="{activeTab: 'userTable'}" class="h-full">
+                    <section x-cloak x-show="activeTab == 'userTable'" class="h-full">
+                        <livewire:users />
+                    </section>
+                    <section x-cloak x-show="activeTab == 'createUser'" class="h-full">
+                        <livewire:create-user />
+                    </section>
+                </div>
             </section>
 
-            <section x-show="activeTab == 'logs'">
+            <!-- logs tab -->
+            <section class="h-full" x-cloak x-show="activeTab == 'logs'">
                 <livewire:logs />
             </section>
 
-            <section x-show="activeTab == 'account'">
+            <!-- account tab -->
+            <section class="h-full" x-cloak x-show="activeTab == 'account'">
                 <livewire:account />
             </section>
         </div>
