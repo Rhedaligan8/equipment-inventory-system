78e841c51d3b68b892f47aed24ce7601eee3c0f0
devken4u <devken4u@gmail.com>
2025-03-23 20:23:32 +0800

    feat: updated users with table sorting, search, and filter

--- app/Http/livewire/Users.php
+++ app/Http/livewire/Users.php
@@ -3,11 +3,84 @@
 namespace App\Http\Livewire;
 
 use Livewire\Component;
+use App\Models\User;
+use Livewire\WithPagination;
 
 class Users extends Component
 {
+    use WithPagination;
+
+    protected $listeners = ['searchEmployeeById', 'searchUnitById'];
+
+    public $perPage = 10;
+    public $username = "";
+    public $orderDirection = "desc";
+    public $orderBy = "created_at";
+    public $roleFilter = "-1";
+    public $statusFilter = "-1";
+
+    public $employee_id = "";
+    public $unit_id = "";
+
+    public function resetFilter()
+    {
+        if ($this->roleFilter != "-1" || $this->statusFilter != "-1") {
+            $this->roleFilter = "-1";
+            $this->statusFilter = "-1";
+        }
+    }
+
+    public function searchEmployeeById($employee_id)
+    {
+        $this->employee_id = $employee_id;
+    }
+    public function searchUnitById($unit_id)
+    {
+        $this->unit_id = $unit_id;
+    }
+
+    public function toggleOrder($orderBy)
+    {
+        if ($this->orderBy == $orderBy) {
+            $this->orderDirection = $this->orderDirection == "asc" ? "desc" : "asc";
+        } else {
+            $this->orderDirection = "desc";
+        }
+        $this->orderBy = $orderBy;
+    }
+
+    public function updated()
+    {
+        $this->resetPage('usersPage');
+    }
+
     public function render()
     {
-        return view('livewire.users')->layout("layouts.dashboard");
+        return view(
+            'livewire.users',
+            [
+                'users' => User::select('users.*')
+                    ->join('infosys.employee', 'infosys.employee.employee_id', '=', 'users.employee_id')
+                    ->join('infosys.unit', 'infosys.unit.unit_id', '=', 'infosys.employee.unit_unit_id')
+                    ->join('infosys.division', 'infosys.division.division_id', '=', 'infosys.unit.unit_div')
+                    ->when($this->employee_id, function ($query) {
+                        $query->where('infosys.employee.employee_id', '=', $this->employee_id);
+                    })
+                    ->when($this->unit_id, function ($query) {
+                        $query->where('infosys.employee.unit_unit_id', '=', $this->unit_id);
+                    })
+                    ->when($this->username, function ($query) {
+                        $query->where('users.username', 'like', $this->username . '%');
+                    })
+                    ->when($this->statusFilter !== '', function ($query) {
+                        $query->where('users.status', '!=', $this->statusFilter);
+                    })
+                    ->when($this->roleFilter !== '', function ($query) {
+                        $query->where('users.role', '!=', $this->roleFilter);
+                    })
+                    ->orderBy($this->orderBy, $this->orderDirection)
+                    ->paginate($this->perPage, ['*'], "usersPage")
+            ]
+        )->layout("layouts.dashboard");
     }
 }
