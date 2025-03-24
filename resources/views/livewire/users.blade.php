1dca6889f9725310fe7b7db09bc7f7f62fcf38d2
devken4u <devken4u@gmail.com>
2025-03-24 18:39:12 +0800

    style: update conditional styling

--- resources/views/livewire/users.blade.php
+++ resources/views/livewire/users.blade.php
@@ -27,7 +27,7 @@
                     <livewire:employee-filter />
                 </div>
                 <div class="dropdown dropdown-end">
-                    <div tabindex="0" role="button" @class(['btn m-1 btn-sm font-bold', 'bg-primary' => $roleFilter !== '-1' || $statusFilter !== '-1',])>
+                    <div tabindex="0" role="button" @class(['btn m-1 btn-sm font-bold', 'bg-primary' => $roleFilter !== '' || $statusFilter !== '',])>
                         <i class="bi bi-funnel-fill" style="font-size: 1rem;"></i>
                         FILTERS
                     </div>
@@ -40,9 +40,9 @@
                             <p class="font-bold">Role</p>
                             <select wire:model.defer="roleFilter"
                                 class="select select-sm focus:ring-0 focus:outline-none">
-                                <option value="-1">All</option>
-                                <option value="0">Admin</option>
-                                <option value="1">Staff</option>
+                                <option value="">All</option>
+                                <option value="1">Admin</option>
+                                <option value="0">Staff</option>
                             </select>
                         </div>
                         <!-- status -->
@@ -50,9 +50,9 @@
                             <p class="font-bold">Status</p>
                             <select wire:model.defer="statusFilter"
                                 class="select select-sm focus:ring-0 focus:outline-none">
-                                <option value="-1">All</option>
-                                <option value="0">Active</option>
-                                <option value="1">Inactive</option>
+                                <option value="">All</option>
+                                <option value="1">Active</option>
+                                <option value="0">Inactive</option>
                             </select>
                         </div>
                         <!-- Filter button -->
@@ -61,115 +61,118 @@
                 </div>
             </div>
         </div>
-        <div class="grow">
-            <table class="table table-zebra">
-                <!-- head -->
-                <thead>
-                    <tr>
-                        <th>#</th>
-                        <th>
-                            <button @class(['text-accent' => $orderBy == 'username', 'hover:underline cursor-pointer hover:text-accent flex items-center']) wire:click="toggleOrder('username')">USERNAME
-                                @if($orderBy == 'username' && $orderDirection == 'asc')
-                                    <i class="bi bi-caret-up-fill" style="font-size: 1rem;"></i>
-                                @elseif($orderBy == 'username' && $orderDirection == 'desc')
-                                    <i class="bi bi-caret-down-fill" style="font-size: 1rem;"></i>
-                                @endif
-                            </button>
-                        </th>
-                        <th>
-                            <button @class(['text-accent' => $orderBy == 'employee.lastname', 'hover:underline cursor-pointer hover:text-accent']) wire:click="toggleOrder('employee.lastname')">NAME
-                                @if($orderBy == 'employee.lastname' && $orderDirection == 'asc')
-                                    <i class="bi bi-caret-up-fill" style="font-size: 1rem;"></i>
-                                @elseif($orderBy == 'employee.lastname' && $orderDirection == 'desc')
-                                    <i class="bi bi-caret-down-fill" style="font-size: 1rem;"></i>
-                                @endif
-                            </button>
-                        </th>
-                        <th>
-                            <button @class(['text-accent' => $orderBy == 'division.division_code', 'hover:underline cursor-pointer hover:text-accent'])
-                                wire:click="toggleOrder('division.division_code')">DIVISION/UNIT
-                                @if($orderBy == 'division.division_code' && $orderDirection == 'asc')
-                                    <i class="bi bi-caret-up-fill" style="font-size: 1rem;"></i>
-                                @elseif($orderBy == 'division.division_code' && $orderDirection == 'desc')
-                                    <i class="bi bi-caret-down-fill" style="font-size: 1rem;"></i>
-                                @endif
-                            </button>
-                        </th>
-                        <th>
-                            <button @class(['text-accent' => $orderBy == 'role', 'hover:underline cursor-pointer hover:text-accent']) wire:click="toggleOrder('role')">ROLE
-                                @if($orderBy == 'role' && $orderDirection == 'asc')
-                                    <i class="bi bi-caret-up-fill" style="font-size: 1rem;"></i>
-                                @elseif($orderBy == 'role' && $orderDirection == 'desc')
-                                    <i class="bi bi-caret-down-fill" style="font-size: 1rem;"></i>
-                                @endif
-                            </button>
-                        </th>
-                        <th>
-                            <button @class(['text-accent' => $orderBy == 'status', 'hover:underline cursor-pointer hover:text-accent']) wire:click="toggleOrder('status')">STATUS
-                                @if($orderBy == 'status' && $orderDirection == 'asc')
-                                    <i class="bi bi-caret-up-fill" style="font-size: 1rem;"></i>
-                                @elseif($orderBy == 'status' && $orderDirection == 'desc')
-                                    <i class="bi bi-caret-down-fill" style="font-size: 1rem;"></i>
-                                @endif
-                            </button>
-                        </th>
-                        <th>
-                            <button @class(['text-accent' => $orderBy == 'created_at', 'hover:underline cursor-pointer hover:text-accent']) wire:click="toggleOrder('created_at')">CREATED AT
-                                @if($orderBy == 'created_at' && $orderDirection == 'asc')
-                                    <i class="bi bi-caret-up-fill" style="font-size: 1rem;"></i>
-                                @elseif($orderBy == 'created_at' && $orderDirection == 'desc')
-                                    <i class="bi bi-caret-down-fill" style="font-size: 1rem;"></i>
-                                @endif
-                            </button>
-                        </th>
-                        <th>
-                            <button @class(['text-accent' => $orderBy == 'updated_at', 'hover:underline cursor-pointer hover:text-accent']) wire:click="toggleOrder('updated_at')">UPDATED AT
-                                @if($orderBy == 'updated_at' && $orderDirection == 'asc')
-                                    <i class="bi bi-caret-up-fill" style="font-size: 1rem;"></i>
-                                @elseif($orderBy == 'updated_at' && $orderDirection == 'desc')
-                                    <i class="bi bi-caret-down-fill" style="font-size: 1rem;"></i>
-                                @endif
-                            </button>
-                        </th>
-                        <th></th>
-                    </tr>
-                </thead>
-                @if ($users->total())
-                    <tbody>
-                        @foreach ($users as $user)
-                            <tr>
-                                <td> {{ $loop->index + 1 }}</td>
-                                <td>{{ $user->username }}</td>
-                                <td>{{ $user->employee->lastname . ', ' . $user->employee->firstname }}</td>
-                                <td class="flex items-center gap-2">
-                                    {{ $user->employee->unit->division->division_code . '/' . $user->employee->unit->unit_code }}
-                                    <div class="tooltip"
-                                        data-tip="{{ $user->employee->unit->division->division_desc }} / {{ $user->employee->unit->unit_desc }}">
-                                        <i class="bi bi-info-circle-fill text-accent"></i>
-                                    </div>
-                                </td>
-                                <td>
-                                    @if($user->role == 1)
-                                        <div class="badge badge-soft badge-warning font-bold">Admin</div>
-                                    @elseif($user->role == 0)
-                                        <div class="badge badge-soft badge-info font-bold">Staff</div>
+        <div class="grow overflow-hidden">
+            <div class="h-full overflow-auto">
+                <table class="table table-zebra table-pin-rows">
+                    <!-- head -->
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>
+                                <button @class(['text-accent' => $orderBy == 'username', 'hover:underline cursor-pointer hover:text-accent flex items-center']) wire:click="toggleOrder('username')">USERNAME
+                                    @if($orderBy == 'username' && $orderDirection == 'asc')
+                                        <i class="bi bi-caret-up-fill" style="font-size: 1rem;"></i>
+                                    @elseif($orderBy == 'username' && $orderDirection == 'desc')
+                                        <i class="bi bi-caret-down-fill" style="font-size: 1rem;"></i>
                                     @endif
-                                </td>
-                                <td>
-                                    @if($user->status == 1)
-                                        <div class="badge badge-soft badge-success font-bold">Active</div>
-                                    @elseif($user->status == 0)
-                                        <div class="badge badge-soft badge-error font-bold">Inactive</div>
+                                </button>
+                            </th>
+                            <th>
+                                <button @class(['text-accent' => $orderBy == 'employee.lastname', 'hover:underline cursor-pointer hover:text-accent'])
+                                    wire:click="toggleOrder('employee.lastname')">NAME
+                                    @if($orderBy == 'employee.lastname' && $orderDirection == 'asc')
+                                        <i class="bi bi-caret-up-fill" style="font-size: 1rem;"></i>
+                                    @elseif($orderBy == 'employee.lastname' && $orderDirection == 'desc')
+                                        <i class="bi bi-caret-down-fill" style="font-size: 1rem;"></i>
                                     @endif
-                                </td>
-                                <td>{{ $user->created_at->format('F j, Y') }}</td>
-                                <td>{{ $user->updated_at->format('F j, Y') }}</td>
-                                <td><i class="bi bi-gear-fill"></i></td>
-                            </tr>
-                        @endforeach
-                    </tbody>
-                @endif
-            </table>
+                                </button>
+                            </th>
+                            <th>
+                                <button @class(['text-accent' => $orderBy == 'division.division_code', 'hover:underline cursor-pointer hover:text-accent'])
+                                    wire:click="toggleOrder('division.division_code')">DIVISION/UNIT
+                                    @if($orderBy == 'division.division_code' && $orderDirection == 'asc')
+                                        <i class="bi bi-caret-up-fill" style="font-size: 1rem;"></i>
+                                    @elseif($orderBy == 'division.division_code' && $orderDirection == 'desc')
+                                        <i class="bi bi-caret-down-fill" style="font-size: 1rem;"></i>
+                                    @endif
+                                </button>
+                            </th>
+                            <th>
+                                <button @class(['text-accent' => $orderBy == 'role', 'hover:underline cursor-pointer hover:text-accent']) wire:click="toggleOrder('role')">ROLE
+                                    @if($orderBy == 'role' && $orderDirection == 'asc')
+                                        <i class="bi bi-caret-up-fill" style="font-size: 1rem;"></i>
+                                    @elseif($orderBy == 'role' && $orderDirection == 'desc')
+                                        <i class="bi bi-caret-down-fill" style="font-size: 1rem;"></i>
+                                    @endif
+                                </button>
+                            </th>
+                            <th>
+                                <button @class(['text-accent' => $orderBy == 'status', 'hover:underline cursor-pointer hover:text-accent']) wire:click="toggleOrder('status')">STATUS
+                                    @if($orderBy == 'status' && $orderDirection == 'asc')
+                                        <i class="bi bi-caret-up-fill" style="font-size: 1rem;"></i>
+                                    @elseif($orderBy == 'status' && $orderDirection == 'desc')
+                                        <i class="bi bi-caret-down-fill" style="font-size: 1rem;"></i>
+                                    @endif
+                                </button>
+                            </th>
+                            <th>
+                                <button @class(['text-accent' => $orderBy == 'created_at', 'hover:underline cursor-pointer hover:text-accent']) wire:click="toggleOrder('created_at')">CREATED AT
+                                    @if($orderBy == 'created_at' && $orderDirection == 'asc')
+                                        <i class="bi bi-caret-up-fill" style="font-size: 1rem;"></i>
+                                    @elseif($orderBy == 'created_at' && $orderDirection == 'desc')
+                                        <i class="bi bi-caret-down-fill" style="font-size: 1rem;"></i>
+                                    @endif
+                                </button>
+                            </th>
+                            <th>
+                                <button @class(['text-accent' => $orderBy == 'updated_at', 'hover:underline cursor-pointer hover:text-accent']) wire:click="toggleOrder('updated_at')">UPDATED AT
+                                    @if($orderBy == 'updated_at' && $orderDirection == 'asc')
+                                        <i class="bi bi-caret-up-fill" style="font-size: 1rem;"></i>
+                                    @elseif($orderBy == 'updated_at' && $orderDirection == 'desc')
+                                        <i class="bi bi-caret-down-fill" style="font-size: 1rem;"></i>
+                                    @endif
+                                </button>
+                            </th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    @if ($users->total())
+                        <tbody>
+                            @foreach ($users as $user)
+                                <tr>
+                                    <td> {{ $loop->index + 1 }}</td>
+                                    <td>{{ $user->username }}</td>
+                                    <td>{{ $user->employee->lastname . ', ' . $user->employee->firstname }}</td>
+                                    <td class="flex items-center gap-2">
+                                        {{ $user->employee->unit->division->division_code . '/' . $user->employee->unit->unit_code }}
+                                        <div class="tooltip"
+                                            data-tip="{{ $user->employee->unit->division->division_desc }} / {{ $user->employee->unit->unit_desc }}">
+                                            <i class="bi bi-info-circle-fill text-accent"></i>
+                                        </div>
+                                    </td>
+                                    <td>
+                                        @if($user->role == 1)
+                                            <div class="badge badge-soft badge-warning font-bold">Admin</div>
+                                        @elseif($user->role == 0)
+                                            <div class="badge badge-soft badge-info font-bold">Staff</div>
+                                        @endif
+                                    </td>
+                                    <td>
+                                        @if($user->status == 1)
+                                            <div class="badge badge-soft badge-success font-bold">Active</div>
+                                        @elseif($user->status == 0)
+                                            <div class="badge badge-soft badge-error font-bold">Inactive</div>
+                                        @endif
+                                    </td>
+                                    <td>{{ $user->created_at->format('F j, Y') }}</td>
+                                    <td>{{ $user->updated_at->format('F j, Y') }}</td>
+                                    <td><i class="bi bi-gear-fill"></i></td>
+                                </tr>
+                            @endforeach
+                        </tbody>
+                    @endif
+                </table>
+            </div>
             @if(!$users->total())
                 <h1 class="font-inter font-bold text-center my-4 text-xl">NO DATA</h1>
             @endif
@@ -181,7 +184,7 @@
                     <p class="text-sm ml-auto font-bold">Page: {{ $users->currentPage() }}</p>
                 @endif
                 <div>
-                    {{ $users->links() }}
+                    {{ $users->onEachSide(1)->links() }}
                 </div>
             </div>
         @endif
