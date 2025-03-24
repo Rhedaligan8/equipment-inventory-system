8002fa147ac34cad06eb31813fe5cbb3b5366760
devken4u <devken4u@gmail.com>
2025-03-24 20:31:15 +0800

    feat: create add location

---
+++ app/Http/livewire/AddLocation.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+use App\Models\Location;
+use App\Models\Log;
+use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Auth;
+
+class AddLocation extends Component
+{
+
+    public $name;
+    public $remarks;
+    public $status = 1;
+
+    protected $rules = [
+        'name' => 'required|max:255|unique:icteiis.locations,name',
+    ];
+
+    protected $messages = [
+        'name.required' => '*Location name is required.',
+        'name.unique' => '*Location is already existing.',
+        'name.max' => '*Location name too long.',
+    ];
+
+    public function create()
+    {
+
+        // force logout user if the role is not admin
+        // if (Gate::denies('access')) {
+        //     Auth::logout();
+        //     return redirect()->route("login");
+        // }
+
+        $this->name = trim($this->name);
+        $this->remarks = trim($this->remarks);
+        $this->status = 1;
+
+        $this->validate();
+
+        $newLocation = Location::create([
+            "name" => $this->name,
+            "remarks" => $this->remarks,
+            "status" => $this->status,
+        ]);
+
+        if ($newLocation) {
+            $this->emit('trigger-toast', 'New location created.', 'success');
+            $this->emit('updateLocationsTable');
+            if ($newLocation) {
+                $message = "New location created - location name: {$this->name}";
+                Log::create([
+                    'user_id' => Auth::user()->user_id,
+                    'type' => 'create',
+                    'message' => $message,
+                ]);
+                $this->emit('updateLogsTable');
+            }
+        }
+        $this->name = "";
+        $this->remarks = "";
+    }
+
+    public function render()
+    {
+        return view('livewire.add-location');
+    }
+}
