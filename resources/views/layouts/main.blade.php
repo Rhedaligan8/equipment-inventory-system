8f0eb59d72005b98bfa6611868e8c8afe2219f65
devken4u <devken4u@gmail.com>
2025-03-24 18:36:04 +0800

    feat: add toast

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -7,6 +7,7 @@
     <link href="{{ asset('css/app.css') }}" rel="stylesheet" />
     <link rel="stylesheet" href="{{ asset('css/bootstrap-icons.css') }}">
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <title>ICTEIIS</title>
     @livewireStyles
 </head>
@@ -19,6 +20,26 @@
 
     @stack('scripts') {{-- For additional scripts --}}
     @livewireScripts
+    <script>
+        const Toast = Swal.mixin({
+            toast: true,
+            position: 'top-start',
+            iconColor: 'white',
+            customClass: {
+                popup: 'colored-toast',
+            },
+            showConfirmButton: false,
+            timer: 3000,
+            timerProgressBar: true,
+        })
+
+        Livewire.on('trigger-toast', (message, type) => {
+            Toast.fire({
+                icon: type,
+                title: message,
+            })
+        });
+    </script>
 </body>
 
 </html>
