ddd64a8c3a7d2e908b112ce816249df40c684c37
devken4u <devken4u@gmail.com>
2025-03-24 18:38:03 +0800

    feat: add gate access

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
+use App\Models\User;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -24,7 +25,14 @@ class AuthServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->registerPolicies();
-
         //
+        Gate::define('access', function (User $user) {
+            $latestUser = User::find($user->user_id); // Get fresh data from the DB
+            if ($latestUser->role === 1) {
+                return true;
+            } elseif ($latestUser->role === 0) {
+                return false;
+            }
+        });
     }
 }
